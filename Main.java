ae71904e9a57f7649c4c3cc036ade71f2d37b9ce
Orange314159 <mratt.robson08@gmail.com>
2025-12-28 22:00:49 -0600

    fixed structure

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		Game game = new Game();
-		game.addPlayer("Red");
-		game.addPlayer("Stupid");
-		game.start();
-		game.playTurn();
-	}
-}
